08326d75064b7976b6b44651a811c75377902f9f
Caracciolo <nathan.caracciolo@free.fr>
2022-04-01 13:24:09 +0200

    refacto

--- src/main.ml
+++ src/main.ml
@@ -110,21 +110,21 @@ let useN = function
     | Move(_, s1) -> SSet.singleton s1
     | Oper(_, _, s1, s2) -> SSet.add s1 (SSet.singleton s2) 
 
-let rec aux_update newest k (regs : string IMap.t) (binds : int SMap.t) free v = 
+let rec aux_update newest k (regs : string IMap.t) (binds : int SMap.t) free = 
     if SSet.is_empty newest then
-        (k, v, free, regs, binds)
+        (k, free, regs, binds)
     else if not (ISet.is_empty free) then 
         let n = SSet.choose newest in 
         let newest = SSet.remove n newest in 
         let f = ISet.choose free in
         let free = ISet.remove f free in
-            aux_update newest k (IMap.add f n regs) (SMap.add n f binds) free v 
+            aux_update newest k (IMap.add f n regs) (SMap.add n f binds) free
     else
         let n = SSet.choose newest in 
         let newest = SSet.remove n newest in 
-            aux_update newest (k + 1) (IMap.add k n regs) (SMap.add n k binds) free v
+            aux_update newest (k + 1) (IMap.add k n regs) (SMap.add n k binds) free
 
-let update (regs : string IMap.t) (binds : int SMap.t) def newest free k v = 
+let update (regs : string IMap.t) (binds : int SMap.t) def newest free k = 
     let r = SMap.find def binds in
     if (SSet.cardinal newest) > 0 then
         let n = SSet.choose newest in 
@@ -132,9 +132,9 @@ let update (regs : string IMap.t) (binds : int SMap.t) def newest free k v =
         let binds = SMap.add n r binds in  
         let binds = SMap.remove def binds in  
         let regs = IMap.add r n regs in  
-            aux_update newest k regs binds free v 
+            aux_update newest k regs binds free
     else   
-        (k, SSet.remove def v, ISet.add (SMap.find def binds) free, regs, binds)
+        (k, ISet.add (SMap.find def binds) free, regs, binds)
 
 let distribute (i : inst) (binds : int SMap.t) (prevbinds : int SMap.t) = 
     let pf = (fun x -> SMap.find x prevbinds) in
@@ -158,48 +158,61 @@ let analyse (instr : inst list) (initials : SSet.t) =
 
     let binds = IMap.fold (fun k v m -> SMap.add v k m) regs SMap.empty in
 
-    let rec cross (v : SSet.t) (r : string IMap.t) (b : int SMap.t) (k : int) (free : ISet.t) = function
+    let rec cross (currentVariables : SSet.t) (r : string IMap.t) (bind : int SMap.t) (k : int) (free : ISet.t) = function
     | [] -> []
-    | x :: subL -> 
-        let def = defN x in 
-        let pre = SSet.diff v (SSet.singleton def) in 
-        let use = useN x in 
-        let newest = SSet.diff (SSet.diff use pre) v in 
-        let pbind = b in 
-        let (k,_,free,r,b) = update r b def newest free k v in 
-        let ninst = (distribute x b pbind) in 
+    | i :: subL -> 
+        let def = defN i in 
+        let pre = SSet.diff currentVariables (SSet.singleton def) in 
+        let use = useN i in 
+        let newest = SSet.diff (SSet.diff use pre) currentVariables in 
+        let previousBind = bind in 
+        let (k,free,r,b) = update r bind def newest free k in 
+        let ninst = (distribute i b previousBind) in 
         ninst :: (cross (SSet.union pre use) r b k free subL)
 
-    in cross initials regs binds k free instr 
+    in 
+    let prog = cross initials regs binds k free instr in 
+    let optiProg = List.filter (fun x -> 
+        match x with 
+        | Move(d,s) -> d <> s 
+        | _ -> true
+    ) (List.rev prog) 
+    in optiProg
+
+let rec setupTest (values : int list) (v : SSet.t) acc = 
+    match values with
+    | [] when SSet.is_empty v -> acc
+    | x :: subL -> 
+        let name = SSet.choose v in
+        let v = SSet.remove name v in 
+            setupTest subL v [(name, x)]
+    | _ -> failwith "Inccorect init data"
+
+let saveProg (path : string) prog = 
+    let saveProg = open_out path in 
+        List.iter (fun x -> output_string saveProg (pprintInst x ^ "\n")) prog
 
 let main() = 
+    let (variables, instrs) = parse "test/slp_630.txt" in 
+    
     let env = setupEnv (SMap.empty) 
         [("A0", 1);("A1", 4);("A2", 5);("A3", 8);("A4", 9);("A5", 2);
         ("B0", 2);("B1", 3);("B2", 6);("B3", 2);("B4", 4);("B5", 0);] 
     in 
 
-    let (variables, instrs) = parse "test/slp_630.txt" in 
-    
     Printf.eprintf "Interprétation sans allocation \n"; 
     let exec = inter instrs env in 
         SMap.iter (fun x v -> Printf.eprintf "%s " (x ^ " = " ^ (string_of_int v))) (SMap.filter (fun x _ -> (String.contains x 'C')) exec);
         Printf.eprintf "\n";
 
-    let prog = analyse instrs variables.var_out in 
-    let optiProg = List.filter (fun x -> 
-        match x with 
-        | Move(d,s) -> d <> s 
-        | _ -> true
-    ) (List.rev prog) in 
+    let newProg = analyse instrs variables.var_out in saveProg "test/slp_630_alloc.txt" newProg;
     
-    (* Code du programme *)
-    (* List.iter (fun x -> Printf.eprintf "%s \n" (pprintInst x)) optiProg; *)
-
     Printf.eprintf "Interprétation avec allocation \n"; 
     let env = setupEnv (SMap.empty) 
         [("r21", 1);("r24", 4);("r7", 5);("r12", 8);("r11", 9);("r8", 2);
         ("r18", 2);("r28", 3);("r29", 6);("r2", 2);("r17", 4);("r27", 0);] 
-    in let exec = inter optiProg env in 
+
+    in let exec = inter newProg env in 
         SMap.iter (fun x v -> Printf.eprintf "%s " (x ^ " = " ^ (string_of_int v))) (SMap.filter (fun x _ -> (getRegistreNumber x) < 6) exec);
         Printf.eprintf "\n"
 ;;
